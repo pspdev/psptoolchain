a2ae0880813c7a12bf33b0cf7b9bac37e0a2173b
Dan Peori <dan.peori@oopo.net>
2011-04-09 20:47:03 -0300

    Fixed the gcc build scripts.

--- scripts/005-gcc-4.5.2-stage2.sh
+++ scripts/005-gcc-4.5.2-stage2.sh
@@ -7,14 +7,23 @@
  ## Download the source code.
  wget --continue ftp://ftp.gnu.org/pub/gnu/gcc/gcc-4.5.2/gcc-4.5.2.tar.bz2
 
+ ## Downlow the library source code.
+ wget --continue ftp://ftp.gmplib.org/pub/gmp-5.0.1/gmp-5.0.1.tar.bz2
+ wget --continue http://www.multiprecision.org/mpc/download/mpc-0.8.2.tar.gz
+ wget --continue http://www.mpfr.org/mpfr-2.4.2/mpfr-2.4.2.tar.bz2
+
  ## Unpack the source code.
  rm -Rf gcc-4.5.2
  tar xfvj gcc-4.5.2.tar.bz2
 
  ## Enter the source directory and patch the source code.
  cd gcc-4.5.2
-
  patch -p1 < ../../patches/gcc-4.5.2-PSP.patch
+
+ ## Unpack the library source code.
+ tar xfvj ../gmp-5.0.1.tar.bz2 && ln -s gmp-5.0.1 gmp
+ tar xfvz ../mpc-0.8.2.tar.gz && ln -s mpc-0.8.2 mpc
+ tar xfvj ../mpfr-2.4.2.tar.bz2 && ln -s mpfr-2.4.2 mpfr
 
  ## Create and enter the build directory.
  mkdir build-psp
