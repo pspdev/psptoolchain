84eb794ba071677b28bb4cc40a10c2b75d5ba5bd
Sam Hegarty <hegarty.sam@gmail.com>
2011-10-24 17:50:08 +1300

    fixes #009

--- depends/check-ncurses.sh
+++ depends/check-ncurses.sh
@@ -3,5 +3,6 @@
 
  ## Check for the ncurses library.
  ls /usr/include/ncurses.h 1> /dev/null ||
+ ls /usr/include/ncurses/ncurses.h 1> /dev/null ||
  ls /usr/local/include/ncurses.h 1> /dev/null ||
  { echo "ERROR: Install ncurses before continuing."; exit 1; }
