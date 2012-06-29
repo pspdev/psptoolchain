1d5f5673fea4a9a36d15cf52c537fc4b5d33c8bb
Scott Marnik <n00neimp0rtant@me.com>
2012-06-29 16:36:51 -0400

    .

--- prepare-mac-os.sh
+++ prepare-mac-os.sh
@@ -7,7 +7,7 @@ if [ -e "/opt/local/bin/port" ]; then
 	sudo port install libusb
 	
 	wget --continue http://downloads.sourceforge.net/project/libusb/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz -O libusb-legacy.tar.gz
-	rm -Rf libusb-legacy && mkdir libusb-legacy && tar --strip-components=1 --directory=psplinkusb -xzf libusb-legacy.tar.gz
+	rm -Rf libusb-legacy && mkdir libusb-legacy && tar --strip-components=1 --directory=libusb-legacy -xzf libusb-legacy.tar.gz
 	cd libusb-legacy
 	./configure && make && make install
 	cd ../
