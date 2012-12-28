c569be3b66eb8d2aafff0691dbc0b1d3c38e772d
Arthur Blot <arthur.blot78@gmail.com>
2012-12-28 17:39:15 +0100

    Fixed psplinkusb script

--- scripts/009-psplinkusb.sh
+++ scripts/009-psplinkusb.sh
@@ -4,21 +4,6 @@
  ## Exit on errors
  set -e
  
- ## Download legacy version of libusb.
- wget --continue http://downloads.sourceforge.net/project/libusb/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Flibusb%2Ffiles%2Flibusb-0.1%2520%2528LEGACY%2529%2F0.1.12%2F&ts=1340988823&use_mirror=voxel -O libusb-legacy.tar.gz
- 
- ## Unpack the source code
- rm -Rf libusb-legacy && mkdir libusb-legacy && tar --strip-components=1 --directory=psplinkusb -xzf libusb-legacy.tar.gz
- 
- ## Enter the source directory.
- cd libusb-legacy
- 
- ## Install legacy libusb.
- ./configure && make && make install
- 
- ## Jump back out.
- cd ../
-
  ## Download the source code.
  wget --continue --no-check-certificate https://github.com/pspdev/psplinkusb/tarball/master -O psplinkusb.tar.gz
 
