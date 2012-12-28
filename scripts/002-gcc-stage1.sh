b30f2d2087066e00c1f6839ce176e9fc6a10153f
Arthur Blot <arthur.blot78@gmail.com>
2012-12-28 17:39:34 +0100

    Fixed linux compilation by updating gmp, mpfr, mpc

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -3,6 +3,9 @@
 
  ## set gcc version
  GCC_VERSION=4.6.3
+ GMP_VERSION=5.1.0
+ MPC_VERSION=1.0.1
+ MPFR_VERSION=3.1.1
 
  ## Exit on errors
  set -e
@@ -11,9 +14,9 @@
  wget --continue ftp://ftp.gnu.org/pub/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2
 
  ## Download the library source code.
- wget --continue ftp://ftp.gmplib.org/pub/gmp-5.0.2/gmp-5.0.2.tar.bz2
- wget --continue http://www.multiprecision.org/mpc/download/mpc-0.8.2.tar.gz
- wget --continue http://www.mpfr.org/mpfr-3.1.0/mpfr-3.1.0.tar.bz2
+ wget --continue ftp://ftp.gmplib.org/pub/gmp-$GMP_VERSION/gmp-$GMP_VERSION.tar.bz2
+ wget --continue http://www.multiprecision.org/mpc/download/mpc-$MPC_VERSION.tar.gz
+ wget --continue http://www.mpfr.org/mpfr-$MPFR_VERSION/mpfr-$MPFR_VERSION.tar.bz2
 
  ## Unpack the source code.
  rm -Rf gcc-$GCC_VERSION
@@ -24,9 +27,9 @@
  patch -p1 < ../../patches/gcc-$GCC_VERSION-PSP.patch
 
  ## Unpack the library source code.
- tar xfj ../gmp-5.0.2.tar.bz2 && ln -s gmp-5.0.2 gmp
- tar xfz ../mpc-0.8.2.tar.gz && ln -s mpc-0.8.2 mpc
- tar xfj ../mpfr-3.1.0.tar.bz2 && ln -s mpfr-3.1.0 mpfr
+ tar xfj ../gmp-$GMP_VERSION.tar.bz2 && ln -s gmp-$GMP_VERSION gmp
+ tar xfz ../mpc-$MPC_VERSION.tar.gz && ln -s mpc-$MPC_VERSION mpc
+ tar xfj ../mpfr-$MPFR_VERSION.tar.bz2 && ln -s mpfr-$MPFR_VERSION mpfr
 
  ## Create and enter the build directory.
  mkdir build-psp
