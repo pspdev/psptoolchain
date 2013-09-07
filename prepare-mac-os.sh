0272869f900b14dcd0d137e45b5b4c24c7960924
uppfinnarn <webmaster@macaronicode.se>
2013-09-07 13:50:12 +0200

    Added Brew support to prepare-mac-os.sh

--- prepare-mac-os.sh
+++ prepare-mac-os.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-if [ -e "/opt/local/bin/port" ]; then
-	sudo port install wget
-	sudo port install libelf
-	sudo port install libmpc
-	sudo port install libusb
-	
+install_libusb() {
 	wget --continue http://downloads.sourceforge.net/project/libusb/libusb-0.1%20%28LEGACY%29/0.1.12/libusb-0.1.12.tar.gz -O libusb-legacy.tar.gz
 	rm -Rf libusb-legacy && mkdir libusb-legacy && tar --strip-components=1 --directory=libusb-legacy -xzf libusb-legacy.tar.gz
 	cd libusb-legacy
 	./configure && make && make install
 	cd ../
 	rm -Rf libusb-legacy && rm libusb-legacy.tar.gz
+}
+
+if [ -e "/opt/local/bin/port" ]; then
+	sudo port install wget
+	sudo port install libelf
+	sudo port install libmpc
+	sudo port install libusb
+	
+	install_libusb()
+elif [ -e "/usr/local/bin/brew" ]; then
+	brew install wget
+	brew install libelf
+	brew install libmpc
+	brew install libusb
+	
+	install_libusb()
 else
-	echo "Go install MacPorts from http://www.macports.org/ first, then we can talk"
+	echo "Go install MacPorts from http://www.macports.org/ or Homebrew from http://brew.sh/ first, then we can talk"
 fi
+
