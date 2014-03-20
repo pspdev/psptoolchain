897f3b9d64f597b738223cc7e90e67be00d5fd01
Martin Charles <martincharles07@gmail.com>
2014-03-20 12:57:56 -0400

    Added ubuntu i386 support

--- depends/check-gmp.sh
+++ depends/check-gmp.sh
@@ -8,5 +8,7 @@
  ls /opt/local/include/gmp.h 1> /dev/null || \
  ls /usr/include/x86_64-linux-gnu/gmp.h 1> /dev/null || \
  ls /usr/local/include/x86_64-linux-gnu/gmp.h 1> /dev/null || \
+ ls /usr/include/i386-linux-gnu/gmp.h 1> /dev/null || \
+ ls /usr/local/include/i386-linux-gnu/gmp.h 1> /dev/null || \
  { echo "ERROR: Install gmp before continuing."; exit 1; }
 
