c64d92503c6fce216fd93d91261d5c29faffe0bf
Johannes Ekberg <uppfinnarn@gmail.com>
2014-06-08 11:15:26 +0200

    Space indentation

--- common.sh
+++ common.sh
@@ -2,62 +2,62 @@
 # Usage: extract <archive>
 function auto_extract
 {
-	path=$1
-	name=`echo $path|sed -e "s/.*\///"`
-	ext=`echo $name|sed -e "s/.*\.//"`
-	
-	echo "Extracting $name..."
-	
-	case $ext in
-		"tar") tar -xf $path ;;
-		"gz"|"tgz") tar -xzf $path ;;
-		"bz2"|"tbz2") tar -xjf $path ;;
-		"zip") unzip $path ;;
-		*) echo "I don't know how to extract $ext archives!"; return 1 ;;
-	esac
-	
-	return 0
+    path=$1
+    name=`echo $path|sed -e "s/.*\///"`
+    ext=`echo $name|sed -e "s/.*\.//"`
+    
+    echo "Extracting $name..."
+    
+    case $ext in
+        "tar") tar -xf $path ;;
+        "gz"|"tgz") tar -xzf $path ;;
+        "bz2"|"tbz2") tar -xjf $path ;;
+        "zip") unzip $path ;;
+        *) echo "I don't know how to extract $ext archives!"; return 1 ;;
+    esac
+    
+    return 0
 }
 
 # Downloads and extracts a file, with some extra checks.
 # Usage: download_and_extract <url> <output?>
 function download_and_extract
 {
-	url=$1
-	name=`echo $url|sed -e "s/.*\///"`
-	outdir=$2
-	
-	# If there are already an extracted directory, delete it, otherwise
-	# reapplying patches gets messy. I tried.
-	[ -d $outdir ] && echo "Deleting old version of $outdir" && rm -rf $outdir
-	
-	# First, if the archive already exists, attempt to extract it. Failing
-	# that, attempt to continue an interrupted download. If that also fails,
-	# remove the presumably corrupted file.
-	[ -f $name ] && auto_extract $name || { wget --continue --no-check-certificate $url -O $name || rm -f $name; }
-	
-	# If the file does not exist at this point, it means it was either never
-	# downloaded, or it was deleted for being corrupted. Just go ahead and
-	# download it.
-	# Using wget --continue here would make buggy servers flip out for nothing.
-	[ -f $name ] || wget --no-check-certificate $url -O $name && auto_extract $name
+    url=$1
+    name=`echo $url|sed -e "s/.*\///"`
+    outdir=$2
+    
+    # If there are already an extracted directory, delete it, otherwise
+    # reapplying patches gets messy. I tried.
+    [ -d $outdir ] && echo "Deleting old version of $outdir" && rm -rf $outdir
+    
+    # First, if the archive already exists, attempt to extract it. Failing
+    # that, attempt to continue an interrupted download. If that also fails,
+    # remove the presumably corrupted file.
+    [ -f $name ] && auto_extract $name || { wget --continue --no-check-certificate $url -O $name || rm -f $name; }
+    
+    # If the file does not exist at this point, it means it was either never
+    # downloaded, or it was deleted for being corrupted. Just go ahead and
+    # download it.
+    # Using wget --continue here would make buggy servers flip out for nothing.
+    [ -f $name ] || wget --no-check-certificate $url -O $name && auto_extract $name
 }
 
 # Clones or updates a Git repository.
 # Usage: clone_git_repo <hostname> <user> <repo>
 function clone_git_repo
 {
-	host=$1
-	user=$2
-	repo=$3
-	
-	# If the target directory exists, assume it's a repository and updating it.
-	# If it's not, or if it's corrupted and fails to pull, just nuke it.
-	[ -d $repo ] && cd $repo && git pull || cd .. && rm -rf $repo
-	
-	# If it does not exist at this point, it was never there in the first place
-	# or it was nuked due to being corrupted. Clone and track master, please.
-	# Attempt to clone over SSH if possible, use anonymous HTTP as fallback.
-	# SSH_ASKPASS=false prevents the script from stopping to ask for auth.
-	[ ! -d $repo ] && SSH_ASKPASS=false git clone --recursive -b master git@$host:$repo.git < /dev/null || SSH_ASKPASS=false git clone --recursive -b master https://$host/$user/$repo.git < /dev/null || return 1
+    host=$1
+    user=$2
+    repo=$3
+    
+    # If the target directory exists, assume it's a repository and updating it.
+    # If it's not, or if it's corrupted and fails to pull, just nuke it.
+    [ -d $repo ] && cd $repo && git pull || cd .. && rm -rf $repo
+    
+    # If it does not exist at this point, it was never there in the first place
+    # or it was nuked due to being corrupted. Clone and track master, please.
+    # Attempt to clone over SSH if possible, use anonymous HTTP as fallback.
+    # SSH_ASKPASS=false prevents the script from stopping to ask for auth.
+    [ ! -d $repo ] && SSH_ASKPASS=false git clone --recursive -b master git@$host:$repo.git < /dev/null || SSH_ASKPASS=false git clone --recursive -b master https://$host/$user/$repo.git < /dev/null || return 1
 }
--- depends/check-dependencies.sh
+++ depends/check-dependencies.sh
@@ -1,96 +1,96 @@
 #!/bin/bash
 
 header_paths=(
-	"/usr/include" \
-	"/usr/local/include" \
-	"/opt/include" \
-	"/opt/local/include" \
-	"/usr/include/$(uname -m)-linux-gnu" \
-	"/usr/local/include/$(uname -m)-linux-gnu"
-	# -- Add more locations here --
+    "/usr/include" \
+    "/usr/local/include" \
+    "/opt/include" \
+    "/opt/local/include" \
+    "/usr/include/$(uname -m)-linux-gnu" \
+    "/usr/local/include/$(uname -m)-linux-gnu"
+    # -- Add more locations here --
 )
 
 aclocal_paths=(
-	"/usr/share/aclocal"
-	"/usr/local/share/aclocal"
+    "/usr/share/aclocal"
+    "/usr/local/share/aclocal"
 )
 
 missing_depends=()
 
 function check_header
 {
-	for place in ${header_paths[@]}; do
-		for name in ${@:2}; do
-			[ -f "$place/$name" ] && return 0
-		done
-	done
-	
-	missing_depends+=($1); return 1
+    for place in ${header_paths[@]}; do
+        for name in ${@:2}; do
+            [ -f "$place/$name" ] && return 0
+        done
+    done
+    
+    missing_depends+=($1); return 1
 }
 
 function check_program
 {
-	binary=${2:-$1}
-	for place in ${PATH//:/ }; do
-		[ -x "$place/$binary" ] || [ -x "$place/$binary.exe" ] && return 0
-	done
-	
-	missing_depends+=($1); return 1
+    binary=${2:-$1}
+    for place in ${PATH//:/ }; do
+        [ -x "$place/$binary" ] || [ -x "$place/$binary.exe" ] && return 0
+    done
+    
+    missing_depends+=($1); return 1
 }
 
 function check_aclocal
 {
-	for place in ${aclocal_paths[@]}; do
-		[ -f "$place/$2" ] && return 0
-	done
-	
-	missing_depends+=($1); return 1
+    for place in ${aclocal_paths[@]}; do
+        [ -f "$place/$2" ] && return 0
+    done
+    
+    missing_depends+=($1); return 1
 }
 
 
 
-check_header	gmp			gmp.h
-check_header	mpc			mpc.h
-check_header	mpfr		mpfr.h
-check_header	libelf		elf.h libelf.h libelf/libelf.h gelf.h libelf/gelf.h
-check_header	libusb		usb.h
-check_header	ncurses		ncurses.h ncurses/ncurses.h
-check_header	readline	readline.h readline/readline.h
-check_header	zlib		zlib.h
+check_header    gmp             gmp.h
+check_header    mpc             mpc.h
+check_header    mpfr            mpfr.h
+check_header    libelf          elf.h libelf.h libelf/libelf.h gelf.h libelf/gelf.h
+check_header    libusb          usb.h
+check_header    ncurses         ncurses.h ncurses/ncurses.h
+check_header    readline        readline.h readline/readline.h
+check_header    zlib            zlib.h
 
 
 
-check_program	git
-check_program	svn
-check_program	wget
-check_program	patch
-check_program	tar
-check_program	unzip
+check_program   git
+check_program   svn
+check_program   wget
+check_program   patch
+check_program   tar
+check_program   unzip
 
-check_program	autoconf
-check_program	automake
-check_program	automake-1.9	# This particular version is needed too
-check_program	libtool
-check_program	cmake
-check_program	make
-check_program	gcc
-check_program	g++
+check_program   autoconf
+check_program   automake
+check_program   automake-1.9    # This particular version is needed too
+check_program   libtool
+check_program   cmake
+check_program   make
+check_program   gcc
+check_program   g++
 
-check_program	sdl-config
-check_program	freetype-config
+check_program   sdl-config
+check_program   freetype-config
 
-check_program	bison
-check_program	flex
-check_program	tclsh
+check_program   bison
+check_program   flex
+check_program   tclsh
 
-check_program	makeinfo
-check_program	doxygen
+check_program   makeinfo
+check_program   doxygen
 
 
 
 if [ ${#missing_depends[@]} -ne 0 ]; then
-	echo "Couldn't find dependencies:"
-	for dep in $missing_depends; do
-		echo "  - $dep"
-	done
+    echo "Couldn't find dependencies:"
+    for dep in $missing_depends; do
+        echo "  - $dep"
+    done
 fi
