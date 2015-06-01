1766dec9189fb75f0662abb9f1263d98bbdf3bca
Victor Xie <ftpiano@gmail.com>
2015-06-01 14:03:49 +0800

    Revised build script of psplinkusb on OS X.
    Added support for Homebrew package manager. psplinkusb will now build with GNU libraries on OS X. This is important for pspsh to build correctly without previous hack, which as made due to the fact that rl_completion_display_matches_hook is not implemented in BSD libedit on OS X.

--- scripts/009-psplinkusb.sh
+++ scripts/009-psplinkusb.sh
@@ -10,10 +10,22 @@
  ## Enter the source directory
  cd psplinkusb
 
- ## MacPorts fix
- export C_INCLUDE_PATH="/opt/local/include"
- export CPLUS_INCLUDE_PATH="/opt/local/include"
- export LIBRARY_PATH="/opt/local/lib"
+ ## Mac OS X fix
+ if [ "$(uname)" == "Darwin" ]; then
+   if [ -d /opt/local/include ] && [ -d /opt/local/lib ]; then # MacPorts
+     export C_INCLUDE_PATH="/opt/local/include"
+     export CPLUS_INCLUDE_PATH="/opt/local/include"
+     export LIBRARY_PATH="/opt/local/lib"
+   elif brew --version 1>/dev/null 2>&1; then # Homebrew
+     HOMEBREW_PREFIX=$(brew --prefix)
+     export C_INCLUDE_PATH="$HOMEBREW_PREFIX/include:$HOMEBREW_PREFIX/opt/readline/include"
+     export CPLUS_INCLUDE_PATH="$C_INCLUDE_PATH"
+     export LIBRARY_PATH="$HOMEBREW_PREFIX/lib:$HOMEBREW_PREFIX/opt/readline/lib"
+   else
+     # Additional package manager/well-known locations? Add them here...
+     echo "WARNING: using libreadline from OS X, this may lead to compilation issues"
+   fi
+ fi
 
  ## Build and install.
  make -f Makefile.clients -j $(num_cpus) clean
