1fff03fe952f6a69773bb3524c815fed3d12ec60
Jakub Kaszycki <jakub@kaszycki.net.pl>
2018-12-16 16:19:42 +0100

    Fix a build error of stage 2 GCC

--- scripts/005-gcc-stage2.sh
+++ scripts/005-gcc-stage2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
  EXTRA_LANGUAGES=',c++,objc,obj-c++'
- EXTRA_CONFIGURE_FLAGS='--enable-cxx-flags="-G0"'
+ EXTRA_CONFIGURE_FLAGS='--enable-cxx-flags=-G0'
  CFLAGS_FOR_TARGET='-G0'
  export CFLAGS_FOR_TARGET
 
