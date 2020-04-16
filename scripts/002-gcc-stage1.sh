a1e7ddd0f988e5521aec726dfa119eb4eb45f20a
Daniel 0xdbeef Zalega <daniel.zalega@gmail.com>
2020-04-16 17:56:37 +0200

    Upated GCC to 9.3

--- scripts/002-gcc-stage1.sh
+++ scripts/002-gcc-stage1.sh
@@ -2,21 +2,23 @@
 # gcc-stage1.sh by Naomi Peori (naomi@peori.ca) customized by yreeen(yreeen@gmail.com)
 
  ## set gcc version
- GCC_VERSION=4.9.3
- GMP_VERSION=5.1.3
- MPC_VERSION=1.0.2
- MPFR_VERSION=3.1.2
+ GCC_VERSION=9.3.0
+ GMP_VERSION=6.1.2
+ MPC_VERSION=1.1.0
+ MPFR_VERSION=4.0.2
+ ISL_VERSION=0.21
 
  ## Exit on errors
  set -e
 
  ## Download the source code if it does not already exist.
- download_and_extract https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.bz2 gcc-$GCC_VERSION
+ download_and_extract https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz gcc-$GCC_VERSION
 
  ## Download the library source code if it does not already exist.
  download_and_extract https://ftp.gnu.org/gnu/gmp/gmp-$GMP_VERSION.tar.bz2 gmp-$GMP_VERSION
  download_and_extract https://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz mpc-$MPC_VERSION
  download_and_extract https://ftp.gnu.org/gnu/mpfr/mpfr-$MPFR_VERSION.tar.bz2 mpfr-$MPFR_VERSION
+ download_and_extract http://isl.gforge.inria.fr/isl-$ISL_VERSION.tar.gz isl-$ISL_VERSION
 
  ## Enter the source directory and patch the source code.
  cd gcc-$GCC_VERSION
@@ -26,13 +28,14 @@
  ln -fs ../gmp-$GMP_VERSION gmp
  ln -fs ../mpc-$MPC_VERSION mpc
  ln -fs ../mpfr-$MPFR_VERSION mpfr
-
+ ln -fs ../isl-$ISL_VERSION isl
+ 
  ## Create and enter the build directory.
  mkdir build-psp
  cd build-psp
 
  ## Configure the build.
- CFLAGS="$CFLAGS -I/opt/local/include" CPPFLAGS="$CPPFLAGS -I/opt/local/include" LDFLAGS="$LDFLAGS -L/opt/local/lib" ../configure --prefix="$PSPDEV" --target="psp" --enable-languages="c" --enable-lto --with-newlib --with-gmp-include="$(pwd)/gmp" --with-gmp-lib="$(pwd)/gmp/.libs" --with-mpfr-include="$(pwd)/../mpfr/src" --with-mpfr-lib="$(pwd)/mpfr/src/.libs" --without-headers --disable-libssp
+ CFLAGS="$CFLAGS -I/opt/local/include" CPPFLAGS="$CPPFLAGS -I/opt/local/include" LDFLAGS="$LDFLAGS -L/opt/local/lib" ../configure --prefix="$PSPDEV" --target="psp" --enable-languages="c" --enable-lto --with-newlib --without-headers --disable-libssp
 
  ## Compile and install.
  make -j $(num_cpus) clean
