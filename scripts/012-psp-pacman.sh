bd21162e0b5804afa6a3be6d0dd201050de7fef0
Wouter Wijsman <wwijsman@live.nl>
2020-05-03 21:49:57 +0200

    Added psp-packman script back in

---
+++ scripts/012-psp-pacman.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# psp-pacman.sh by Wouter Wijsman (wwijsman@live.nl)
+
+ ## clear CC and CXX
+ unset CC
+ unset CXX
+
+ ## Exit on errors
+ set -e
+
+ PACMAN_VERSION="5.2.1"
+
+ ## Download the source code
+ download_and_extract https://sources.archlinux.org/other/pacman/pacman-${PACMAN_VERSION}.tar.gz pacman-${PACMAN_VERSION}
+ 
+ ## Enter the source directory
+ cd pacman-${PACMAN_VERSION}
+
+ ## Apply patch
+ patch -p1 < ../../pacman-${PACMAN_VERSION}.patch
+
+ ## Build
+ ./configure --prefix=${PSPDEV} --with-buildscript=PSPBUILD --with-root-dir=${PSPDEV}/psp --program-prefix="psp-" --disable-doc
+ make
+ make install
+ 
+ ## Overwrite config files
+ cp -f ../../makepkg.conf ${PSPDEV}/etc/makepkg.conf
+ cp -f ../../pacman.conf ${PSPDEV}/etc/pacman.conf
