a59cda01fa668fcc6e471f4a979385e6a4d5f1d6
Wouter Wijsman <wwijsman@live.nl>
2020-05-12 23:18:12 +0200

    Add dependencies for pacman

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Install build dependencies
-sudo apt-get install $@ autoconf automake bison bzip2 cmake doxygen flex g++ gcc git gzip libelf-dev libgmp-dev libmpfr-dev libncurses5-dev libreadline-dev libtool-bin libusb-dev make mpc patch pkg-config subversion tar tcl texinfo unzip wget xz-utils
+sudo apt-get install $@ \
+  autoconf automake bison bzip2 cmake doxygen flex g++ gcc git gzip \
+  libarchive-dev libcurl4-openssl-dev libelf-dev libgmp-dev libmpfr-dev \
+  libncurses5-dev libreadline-dev libssl-dev libtool-bin libusb-dev make mpc \
+  patch pkg-config python3 python3-venv subversion tar tcl texinfo unzip wget \
+  xz-utils
