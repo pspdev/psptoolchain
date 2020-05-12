c241f36f190470cbb5c52949196e1c338b819b24
Wouter Wijsman <wwijsman@live.nl>
2020-05-12 20:48:22 +0200

    Add pkg-config

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install build dependencies
-sudo apt-get install $@ autoconf automake bison bzip2 cmake doxygen flex g++ gcc git gzip libelf-dev libgmp-dev libmpfr-dev libncurses5-dev libreadline-dev libtool-bin libusb-dev make mpc patch subversion tar tcl texinfo unzip wget xz-utils
+sudo apt-get install $@ autoconf automake bison bzip2 cmake doxygen flex g++ gcc git gzip libelf-dev libgmp-dev libmpfr-dev libncurses5-dev libreadline-dev libtool-bin libusb-dev make mpc patch pkg-config subversion tar tcl texinfo unzip wget xz-utils
