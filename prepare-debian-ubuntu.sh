ddd94a0550fa3282471b2dafe7c05994524c7951
Wouter Wijsman <wwijsman@live.nl>
2020-05-13 00:24:03 +0200

    Removed gmp, mpfr and mpc

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -3,7 +3,6 @@
 # Install build dependencies
 sudo apt-get install $@ \
   autoconf automake bison bzip2 cmake doxygen flex g++ gcc git gzip \
-  libarchive-dev libcurl4-openssl-dev libelf-dev libgmp-dev libmpfr-dev \
-  libncurses5-dev libreadline-dev libssl-dev libtool-bin libusb-dev make mpc \
-  patch pkg-config python3 python3-venv subversion tar tcl texinfo unzip wget \
-  xz-utils
+  libarchive-dev libcurl4-openssl-dev libelf-dev libncurses5-dev \
+  libreadline-dev libssl-dev libtool-bin libusb-dev make patch pkg-config \
+  python3 python3-venv subversion tar tcl texinfo unzip wget xz-utils
