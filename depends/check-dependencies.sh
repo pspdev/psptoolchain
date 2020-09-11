1c1c43d7bd00a1d78d9e1e2050cb96a91ccfcf63
Jon Daniel <jopadan@ctemplar.com>
2020-09-11 13:10:48 +0000

    Use pkg-config to detect include directory

--- depends/check-dependencies.sh
+++ depends/check-dependencies.sh
@@ -14,19 +14,50 @@ header_paths=(
 
 missing_depends=()
 
-function check_header
+function check_header_path
 {
+	for header_path in ${header_paths[@]}; do
+    		if [ "$1" == "${header_path}" ] ; then
+			return 1
+		fi
+	done
+	header_paths+=( "$1" )
+	return 0
+}
+
+function check_header 
+{
+    package_name=`pkg-config --list-package-names | grep $1`
+    cflags=`pkg-config ${package_name} --cflags-only-I`
+    includedir=`pkg-config --variable=includedir ${package_name}`
+
+    check_header_path ${includedir}
+
+    for place in ${cflags[@]}; do
+    	check_header_path "${place#-I}"
+    done
+
     for place in ${header_paths[@]}; do
         for name in ${@:2}; do
             [ -f "$place/$name" ] && return 0
         done
     done
-    
+
     missing_depends+=($1); return 1
 }
 
 function check_header_nosys
 {
+    package_name=`pkg-config --list-package-names | grep $1`
+    cflags=`pkg-config ${package_name} --cflags-only-I`
+    includedir=`pkg-config --variable=includedir ${package_name}`
+
+    check_header_path ${includedir}
+
+    for place in ${cflags[@]}; do
+    	check_header_path "${place#-I}"
+    done
+
     for place in ${header_paths[@]}; do
         if [ "${place:0:12}" != "/usr/include" ]; then
             for name in ${@:2}; do
@@ -54,7 +85,7 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 
 check_header    libelf          elf.h libelf.h libelf/libelf.h gelf.h libelf/gelf.h
-check_header    libusb          usb.h
+check_header    libusb          libusb.h
 check_header    ncurses         ncurses.h ncurses/ncurses.h
 check_header    zlib            zlib.h
 check_header    libcurl         curl/curl.h
@@ -86,6 +117,8 @@ check_program   which
 check_program   makeinfo
 check_program   doxygen
 
+check_program   sdl-config
+
 check_program   python3
 
 # Sometimes things will be a little different on Mac OS X...
@@ -93,7 +126,7 @@ if [ "$(uname)" == "Darwin" ]; then
     # readline should be checked carefully on OS X to save us from being
     # fooled by BSD libedit.
     # libarchive and openssl are keg-only
-    if command -v brew 1>/dev/null 2>&1; then
+    if brew --version 1>/dev/null 2>&1; then
         header_paths+=("`brew --prefix`/opt/readline/include")
         header_paths+=("`brew --prefix`/opt/libarchive/include")
         header_paths+=("`brew --prefix`/opt/openssl/include")
