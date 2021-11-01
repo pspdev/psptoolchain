07daab80af8c4aa031ab47054897f7d0c626ce09
Jon Daniel <jondaniel879@gmail.com>
2021-11-01 05:56:13 +0100

    change numerical boolean values

--- prepare-mac-os.sh
+++ prepare-mac-os.sh
@@ -13,7 +13,7 @@ install_libusb() {
 show_help() {
 	echo "Use '-b' or '--brew' to install packages with Homebrew, or use '-p' or '--port' to install with MacPorts."
 	echo "If left unspecified, tries Homebrew first, then MacPorts."
-	exit 1
+	exit $(false)
 }
 
 if [ $# -gt 1 ]; then
@@ -22,14 +22,14 @@ if [ $# -gt 1 ]; then
 fi
 
 # default is autodetection
-try_brew=1
-try_port=1
+try_brew=$(true)
+try_port=$(true)
 if [ $# -eq 1 ]; then
 	case "$1" in
 	-b|--brew)
-		try_port=0 ;;
+		try_port=$(false) ;;
 	-p|--port)
-		try_brew=0 ;;
+		try_brew=$(false) ;;
 	*)
 		show_help ;;
 	esac
@@ -37,32 +37,32 @@ fi
 
 if ! [ -e "/usr/local/bin/brew" -o -e "/opt/local/bin/port" ]; then
 	echo "Go install Homebrew from http://brew.sh/ or MacPorts from http://www.macports.org/ first, then we can talk!"
-	exit 1
+	exit $(false)
 fi
 
 # sanity checks
-if [ $try_brew -eq 1 -a ! -e "/usr/local/bin/brew" ]; then
+if [ $try_brew -eq $(true) -a ! -e "/usr/local/bin/brew" ]; then
 	echo "Not trying Homebrew, because it is not installed."
-	try_brew=0
+	try_brew=$(false)
 fi
-if [ $try_port -eq 1 -a ! -e "/opt/local/bin/port" ]; then
+if [ $try_port -eq $(true) -a ! -e "/opt/local/bin/port" ]; then
 	echo "Not trying MacPorts, because it is not installed."
-	try_port=0
+	try_port=$(false)
 fi
-if [ $try_brew -eq 0 -a $try_port -eq 0 ]; then
+if [ $try_brew -eq $(false) -a $try_port -eq $(false) ]; then
 	echo "Invalid package manager specified. Maybe use autodetection."
-	exit 1
+	exit $(false)
 fi
 
 # actual installation
-if [ $try_brew -eq 1 ]; then
+if [ $try_brew -eq $(true) ]; then
 	CURRENT_USER=$(stat -f '%Su' /dev/console)
 	sudo -u $CURRENT_USER brew install autoconf automake cmake doxygen gnu-sed libelf libtool libusb libusb-compat pkg-config wget xz
-	exit
+	exit $(true)
 fi
-if [ $try_port -eq 1 ]; then
+if [ $try_port -eq $(true) ]; then
 	sudo port install autoconf automake cmake doxygen gsed libelf libtool libusb pkgconfig wget xz
 
 	install_libusb
-	exit
+	exit $(true)
 fi
